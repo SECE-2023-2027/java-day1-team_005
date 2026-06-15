29428702ec4e306697c57c7fee603994fe44a80b
Sanjith S <sanjithsece@gmail.com>
2026-06-15 20:37:45 +0530

    changes

--- Inttobi.java
+++ Inttobi.java
@@ -2,14 +2,11 @@ import java.util.Scanner;
 
 public class Inttobi {
 
-    public static int check(int n){
-        
-    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
         System.out.println("Inbuild function :"+Integer.toBinaryString(n));
-        System.out.print("Without inbuild :"+ check());
+        
     }
 }
