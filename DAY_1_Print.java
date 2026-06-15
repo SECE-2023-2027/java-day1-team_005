c9b2b2362537c8f630c45fe970265c837b60580f
Madhesh Kanna <madheshkanna972@gmail.com>
2026-06-15 15:30:27 +0530

    Question 1

---
+++ DAY_1_Print.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.util.jar.Attributes.Name;
+
+class DAY_1_Print{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name=sc.nextLine();
+        System.out.println("HELLO");
+        System.out.println(name);
+    }
+}
